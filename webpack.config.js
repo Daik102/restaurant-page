be0c3bf88a324519937bfc99105f88b1716b0bfe
Daik <fruit@kd6.so-net.ne.jp>
2025-07-12 21:59:23 +0900

    Add a rule for markdown-loader

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,17 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.md$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+          {
+            loader: "markdown-loader",
+          },
+        ],
+      },
     ],
   },
 };
